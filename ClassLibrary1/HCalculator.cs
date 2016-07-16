883902237bb50438c678f60f3fae4dd026b27871
Adam25T <turner.adam025@gmail.com>
2016-07-16 14:19:09 -0400

    Refactoring in progress
    Added some new classes

---
+++ ClassLibrary1/HCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class HCalculator
+    {
+        private List<double> hDifferentials = new List<double>();
+        
+        public HCalculator(List<double> list) {
+            hDifferentials = list;
+        }
+
+        private double computeIndex(List<double> diffList, int upTo) {
+            double runningTotal = 0;
+            double average = 0;
+
+            for (int i = 0; i < upTo; i++) {
+                runningTotal = runningTotal + diffList[i];
+            }
+            average = (runningTotal / upTo) * .96;
+            double truncated = Math.Truncate(average * 10) / 10;
+
+            return truncated;
+        }
+
+        public double getIndex(int upTo) {
+            return computeIndex(hDifferentials, upTo);
+        }
+
+
+
+
+    }
+}
---
+++ ClassLibrary2/ScoreReader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ScoreFileReader
+{
+    public class ScoreReader
+    {
+        private List<double> differentials = new List<double>();
+        private int totalRounds { get; }
+        public List<double> getDifferentials() {
+            differentials.Sort();
+            return differentials;
+        }
+
+        private double computeHandicapDiff(double score, double rating, double slope) {
+            return ((score - rating) * 113) / slope;
+        }
+
+
+
+
+        public ScoreReader() {
+            Console.WriteLine("Enter file path for scores: ");
+            string filePath = Console.ReadLine();
+            string dafilePath = "C:\\Users\\Manifest\\Documents\\GitHub\\HandicapCalculator\\scores_text.txt";
+            try {
+                using (StreamReader sr = new StreamReader(dafilePath)) {
+                    string valueLine;
+                    double score;
+                    double usgaRating;
+                    double courseSlope;
+
+                    while ((valueLine = sr.ReadLine()) != null) {
+                        //read the line in
+                        String[] stringValues = valueLine.Split(' ');
+                        //convert to doubles
+                        score = Convert.ToDouble(stringValues[0]);
+                        usgaRating = Convert.ToDouble(stringValues[1]);
+                        courseSlope = Convert.ToDouble(stringValues[2]);
+                        //calculate differential and store in list
+                        differentials.Add(Math.Round(computeHandicapDiff(score, usgaRating, courseSlope), 1));
+
+                    }
+                }
+            }
+            catch (FileNotFoundException) {
+                Console.WriteLine("Score reader could not find the file you specified...");
+            }
+            totalRounds = differentials.Count;
+        }
+    }
+}
